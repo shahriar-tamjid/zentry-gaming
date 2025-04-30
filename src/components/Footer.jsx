a41a3f36f8e6292b324e0d6c144a6a24b122e87f
shahriar-tamjid <shahriartamjid@gmail.com>
2025-04-30 17:22:09 +0600

    Finished Development

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,16 @@ const socialLinks = [
   { href: "https://medium.com", icon: <FaMedium /> },
 ];
 
+const today = new Date();
+const currentYear = today.getFullYear();
+// console.log(currentYear); // This will output the current year (e.g., 2025)
+
 const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          © Zentry {currentYear}. All rights reserved
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
